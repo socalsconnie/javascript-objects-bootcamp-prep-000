8356792e0d2610e23d831cf65a702e8529201fc3
alison.theabo@gmail.com <alison.theabo@gmail.com>
2018-04-25 20:20:14 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = new Object({ Chromeo: "Come Alive"  })
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign({}, playlist, { [artistName]: songTitle } )
+  playlist[artistName] = songTitle
 }
 
 function removeFromPlaylist(playlist, artistName) {
